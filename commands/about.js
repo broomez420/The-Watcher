132dfaa74c0a53408748b5704962cc0569a3bd0f
broomez420 <34385260+broomez420@users.noreply.github.com>
2019-01-10 14:17:38 +0000

    Update about.js

--- commands/about.js
+++ commands/about.js
@@ -10,7 +10,7 @@ module.exports.run = async (bot, message, args) => {
         .addField("What can this bot do", "this bot can do lots")
 	.addField("Moderation", "kick, ban, warn, tempmute, addrole, removerole")
 	.addField("Fun", "8ball, levels, coins, random dog pics, random cat pics")
-	.addField("Economy", "gives coin at random intervals no value yet")
+	.addField("General", "built in url shortner")
 	.addField("Logs", "logs reports, warnings, kicks, bans")
 	.addField("Other", "shows users who join, leave.. shows when channel is created, deleted..")
         .addField("Created by", bot.users.get("392082408050130945"));
