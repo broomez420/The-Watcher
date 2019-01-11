2968b6f179e83038755f2a7e65c179cd480195bf
broomez420 <34385260+broomez420@users.noreply.github.com>
2019-01-11 21:08:38 +0000

    Update index.js
    added an invite tracking module

--- index.js
+++ index.js
@@ -71,6 +71,24 @@ bot.on('guildMemberAdd', member => {
   console.log(`${member}`, "has joined" + `${member.guild.name}`)
 
 });
+// invite module
+bot.on('guildMemberAdd', member => {
+  // To compare, we need to load the current invite list.
+  member.guild.fetchInvites().then(guildInvites => {
+    // This is the *existing* invites for the guild.
+    const ei = invites[member.guild.id];
+    // Update the cached invites for the guild.
+    invites[member.guild.id] = guildInvites;
+    // Look through the invites, find the one for which the uses went up.
+    const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
+    // This is just to simplify the message being sent below (inviter doesn't have a tag property)
+    const inviter = client.users.get(invite.inviter.id);
+    // Get the log channel (change to your liking)
+    const inviteChannel = member.guild.channels.find(channel => channel.name === "invites");
+    // A real basic message with the information we need. 
+    logChannel.send(`${member.user.tag} joined using invite code ${invite.code} from ${inviter.tag}. Invite was used ${invite.uses} times since its creation.`);
+  });
+});
 
 // leave member message module
 bot.on('guildMemberRemove', member => {
