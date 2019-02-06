e9af6e1649149aa16413c68a1abeeded7ec64029
broomez420 <34385260+broomez420@users.noreply.github.com>
2019-02-06 23:03:00 +0000

    Create omen-exchanges.js

---
+++ commands/omen-exchanges.js
@@ -0,0 +1,44 @@
+const Discord = require('discord.js');
+
+
+exports.run = (client, message, args) => {
+  message.channel.send({embed: {
+    color: 3447003,
+    author: {
+      name: client.user.username,
+      icon_url: client.user.avatarURL
+    },
+    title: "OMEN",
+    url: "https://cryptofightclubco.in",
+    description: "OMEN Exchanges.",
+    fields: [
+      {
+        name: "Graviex",
+        value: "You can check it 👉 [HERE](https://graviex.net/markets/omenbtc) 👈"
+      },
+      {
+        name: "STEX",
+        value: "You can check it 👉 [HERE](https://app.stex.com/en/basic-trade/pair/BTC/OMEN/1D) 👈"
+      }, 
+    ],
+    timestamp: new Date(),
+    footer: {
+      icon_url: client.user.avatarURL,
+      text: "© Broomez420 2018-2019"
+    }
+  }
+});
+        };
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'omen-exchanges',
+  description: 'shows OMENs exchanges.',
+  usage: 'omen-exchanges'
+};
