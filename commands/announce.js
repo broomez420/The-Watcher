05e7f8e1986ba048de88284cf0aea82ba6be0cfc
broomez420 <34385260+broomez420@users.noreply.github.com>
2019-02-09 02:41:42 +0000

    Update announce.js

--- commands/announce.js
+++ commands/announce.js
@@ -1,26 +1,29 @@
 const Discord = require('discord.js');
 
 module.exports.run = async (bot, message, args) => {
-//     if (message.member.hasPermission("ADMINISTRATOR")) {
-//     if (message.content.startsWith('-announce')) {
-//         let rest_of_the_string = message.content.slice('-announce'.length); //removes the first part
-//         let array_of_arguments = rest_of_the_string.split(','); //[title, description, link, image]
-//         const embed = new Discord.RichEmbed()
-//             .setTitle(array_of_arguments[0])
-//             .setDescription(array_of_arguments[1])
-//             .setImage(array_of_arguments[3])
-//             .setURL(array_of_arguments[2])
-//             .addField("Text", true)
-//             .setThumbnail("https://i.imgur.com/Rmiwd1j.png")
-//             .setColor(0x00AE86)
-//             .setFooter("Footer", array_of_arguments[3])
-//             .setTimestamp();
-//            message.channel.send("@everyone");
-//            message.channel.send({embed});
+    if (message.member.hasPermission("ADMINISTRATOR")) {
+    if (message.content.startsWith('-announce')) {
+        let rest_of_the_string = message.content.slice('-announce'.length); //removes the first part
+        let array_of_arguments = rest_of_the_string.split(','); //[title, description, link, image]
+	let channel = message.mentions.channels();
+        let 📣announcements📣 = args.slice(1).join(" ");
+        const embed = new Discord.RichEmbed()
+            .setTitle(array_of_arguments[0])
+            .setDescription(array_of_arguments[1])
+            .setImage(array_of_arguments[3])
+            .setURL(array_of_arguments[2])
+            .addField("Text", true)
+            .setThumbnail("https://i.imgur.com/Rmiwd1j.png")
+            .setColor(0x00AE86)
+            .setFooter("Footer", array_of_arguments[3])
+            .setTimestamp();
+           message.channel.send("@everyone");
+           message.channel.send({embed});
+	   message.channel.send(📣announcements📣);
 	
-//         }
-//     };
-// }
+        }
+    };
+}
 	
 //      if (message.member.hasPermission("ADMINISTRATOR")) {
 //       let args = message.content.split(" ").slice(1).join(" ");
@@ -40,8 +43,7 @@ module.exports.run = async (bot, message, args) => {
 //            footer: {
 //              icon_url: message.author.avatarURL,
 //              text: message.author.username
-// 		   let channel = message.mentions.channels();
-//         let 📣announcements📣 = args.slice(1).join(" ");
+// 		   
 //         channel.send(📣announcements📣);
 //            }
 //          }
@@ -49,21 +51,7 @@ module.exports.run = async (bot, message, args) => {
 //    }
 //  }
 	
-	    let messageArray = message.content.split(" ");
-    let command = messageArray[0];
-    let args = messageArray.slice(1);
 
-    if(message.channel.type === "dm") return;
-
-    if(!message.content.startsWith('-')) return;
-
-    if(command === '-announce') {
-        let channel = message.mentions.channels();
-        let 📣announcements📣 = args.slice(1).join(" ");
-
-        channel.send(📣announcements📣);
-    }
-}
 module.exports.help = {
         name: "announce" 
 }
