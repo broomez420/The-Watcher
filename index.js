74bf9de59a2847fc289f255470627da74d977bdc
broomez420 <34385260+broomez420@users.noreply.github.com>
2019-02-16 16:47:02 +0000

    Update index.js

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ fs.readdir("./commands/", (err, files) => {
 bot.on('ready', () => {
   setInterval(async ()=>{
       
-      let textList = ['How to Manage','lurking','Learning','Protecting `${bot.guilds.size}` servers']
+      let textList = ['How to Manage','lurking','Learning','Protecting `5` servers']
       var text = textList[Math.floor(Math.random() * textList.length)];
       bot.user.setActivity(text , { type: 'WATCHING' })
   },60000) // milliseconds
