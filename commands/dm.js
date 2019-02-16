e1d0bb9e53108918e5b33ea8ffee217938df1ae5
broomez420 <34385260+broomez420@users.noreply.github.com>
2019-02-16 20:20:27 +0000

    Create dm.js

---
+++ commands/dm.js
@@ -0,0 +1,19 @@
+const Discord = require('discord.js');
+
+module.exports.run = async (bot, message, args) => {
+  
+   let dUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
+   if (!dUser) return message.channel.send("Can't find user!")
+   if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply("You can't you that command!")
+   let dMessage = args.join(" ").slice(22);
+   if(dMessage.length < 1) return message.reply('You must supply a message!')
+
+     dUser.send(`${dUser} <@!528992402132959244> sent you: ${dMessage}, Please note i can not see what you sent back if you want to talk to me you can talk to me in one of the server i protect`)
+
+     message.author.send(`${message.author} You have sent your message to ${dUser}`)
+
+  }
+  
+  module.exports.help = {
+  name:"dm"
+}
