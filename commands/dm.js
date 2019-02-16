6904949bcb4cd03cab6aec3e4e0418ffb4aba8ca
broomez420 <34385260+broomez420@users.noreply.github.com>
2019-02-16 20:34:41 +0000

    Update dm.js

--- commands/dm.js
+++ commands/dm.js
@@ -4,7 +4,7 @@ module.exports.run = async (bot, message, args) => {
   
    let dUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
    if (!dUser) return message.channel.send("Can't find user!")
-   if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply("You can't you that command!")
+   if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply("You can't use that command!")
    let dMessage = args.join(" ").slice(22);
    if(dMessage.length < 1) return message.reply('You must supply a message!')
 
