e36a34062678061d100bf8fb51b6231559981cef
broomez420 <34385260+broomez420@users.noreply.github.com>
2019-02-20 21:24:28 +0000

    Update omeninfo.js

--- commands/omeninfo.js
+++ commands/omeninfo.js
@@ -49,7 +49,7 @@ exports.run = (client, message, args) => {
     timestamp: new Date(),
     footer: {
       icon_url: client.user.avatarURL,
-      text: "© broomez420 2018-2019"
+      text: "© OmenCoin 2018-2019"
     }
   }
 });
