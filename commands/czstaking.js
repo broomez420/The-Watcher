835591e266ff3ae8e3f3ad1b29f24015e1ae947a
broomez420 <34385260+broomez420@users.noreply.github.com>
2019-03-06 00:37:31 +0000

    Create czstaking.js

---
+++ commands/czstaking.js
@@ -0,0 +1,126 @@
+const Discord = require('discord.js');
+
+
+exports.run = (client, message, args) => {
+  message.channel.send({embed: {
+    color: 3447003,
+    author: {
+      name: client.user.username,
+      icon_url: client.user.avatarURL
+    },
+    title: "CoinZone",
+    url: "https://discord.gg/mVArCEt",
+    description: "CoinZone Staking pool info",
+    fields: [
+      {
+        name: "**__Staking Pools__**",
+        value: "Here is the updated Staking pool information."
+      },
+     
+      {
+        name: "**__EZY POS__**",
+        value: "[Link](https://db.tt/Zt1ItpJdGa)"
+      },
+      
+      {
+        name: "**__CRYPTONODES POS__**",
+        value: "[Link](https://db.tt/lf99E0K5ta)"
+      },
+      
+      {
+        name: "**__CRYPTO FIGH CLUB POS__**",
+        value: "[Link](https://db.tt/HWS0XTJImx)"
+      },
+      
+      {
+        name: "**__GOCASH POS__**",
+        value: "[Link](https://db.tt/1zrhhvNknQ)"
+      },
+      
+      {
+        name: "**__ZOOMBA POS__**",
+        value: "[Link](https://db.tt/lXrgNhpG1f)"
+      },
+      
+      {
+        name: "**__x42 POS__**",
+        value: "[Link](https://db.tt/V16seiR5eF)"
+      },
+      
+      {
+        name: "**__OMEN POS__**",
+        value: "[Link](https://db.tt/A8RASLvWDP)"
+      },
+      
+      {
+        name: "**__THROAT PUNCH COIN POS__**",
+        value: "[Link](https://db.tt/7HfG2QSTTF)"
+      },
+      
+            {
+        name: "**__PENGUIN POS__**",
+        value: "[Link](https://db.tt/XkCAMVAUJX)"
+      },
+      
+      {
+        name: "**__RATCOIN POS__**",
+        value: "[Link](https://db.tt/jnqU2l7Jyq)"
+      },
+      
+      {
+        name: "**__E-SPORT BETTING COIN POS__**",
+        value: "[Link](https://db.tt/GZJWk88ReP)"
+      },
+      
+      {
+        name: "**__SHUTTLECOIN POS__**",
+        value: "[Link](https://db.tt/mhfXJTAJLU)"
+      },
+      
+            {
+        name: "**__ROCKETCOIN POS__**",
+        value: "[Link](https://db.tt/UGmh8BaM1K)"
+      },
+      
+            {
+        name: "**__ELECTRA COIN__**",
+        value: "[Link](https://db.tt/hOoB2C0SvC)"
+      },
+      
+      {
+        name: "**__VIP-CORE POS__**",
+        value: "[Link](https://db.tt/rK8gTPaJwN)"
+      },
+      
+      {
+        name: "**__ALLSAFE POS__**",
+        value: "[Link](https://db.tt/WGV1hrYTkI)"
+      },
+      
+      {
+        name: "**__XIND POS__**",
+        value: "[Link](https://db.tt/e3XimUDvTA)"
+      },
+
+    ],
+    timestamp: new Date(),
+    footer: {
+      icon_url: client.user.avatarURL,
+      text: "© CoinZone 2018-2019"
+    }
+  }
+});
+        };
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'czstaking',
+  description: 'shows CoinZone's staking pool info.',
+  usage: 'czstaking'
+};
